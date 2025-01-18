afd35c9a8e7cd937c032e0642ea6bcb4f96d8825
rohit@9370 <rohitpro678@gmail.com>
2025-01-18 20:18:50 +0530

    update the home layout

--- App.js
+++ App.js
@@ -1,28 +1,35 @@
-import { View, Text, SafeAreaView, BackHandler } from 'react-native'
+import { View, Text, SafeAreaView, StyleSheet ,StatusBar} from 'react-native'
 import React from 'react'
+
 
 const App = () => {
   return (
-    <SafeAreaView style={styles.maincontiner}>
-        <View style={styles.container}>
-         <Text>Hello search here</Text>
+    <SafeAreaView style={styles.Container}>
+     <StatusBar style='auto'/>
+        <View style={styles.search}>
+         <Text> search here</Text>
         </View>
-        <View style={styles.main}> 
-        <Text>Miain contenet</Text>
+        <View style={styles.list}> 
+        <Text>Miain List</Text>
         </View>
     </SafeAreaView>
   )
 }
 const styles = StyleSheet.create({
-  container:
-  {
+  Container:{
     flex:1,
-    height:'30%',
-    width:'100%'
   },
-  main:{
-    flex:1,
-    backgroundcolour:'yellow'
+  search:
+  {
+    padding:12,
+    height:'5%',
+    width:'100%',
+    backgroundColor:'yellow'
+
+  },
+  list:{
+   flex:1,
+   backgroundColor:'tomato'
   }
 
 })
